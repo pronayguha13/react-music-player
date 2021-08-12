bb451c8cb0c2fbcd706ebb3c27997e8d1543dd36
Pronay Guha <pronayguha13@gmail.com>
2021-08-12 16:29:04 +0530

    Fix y axis overflow on empty screen Fixes #15

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
-import React, { useContext } from "react";
+import React from "react";
+
 import TrackList from "./components/Tracklist";
-import AddTrackButton from "./components/Controller";
-import AudioPlayer from "react-h5-audio-player";
+import Controller from "./components/Controller";
 import Navbar from "./components/Header/index.jsx";
 
-import { MusicContext } from "./global/MusicContext";
+import "./global.css";
 
 const App = () => {
-  const { trackList, error, nowPlaying, autoNextTrackSelector } =
-    useContext(MusicContext);
-
   return (
-    <div className="w-100">
+    <div className="App">
       <Navbar />
-      {trackList.length || error !== null ? (
-        <TrackList />
-      ) : (
-        <h3
-          style={{
-            textAlign: "center",
-            color: "#fff",
-            paddingTop: "50%",
-            paddingBottom: "50%",
-            background: "#002651",
-            marginBottom: 0,
-          }}
-        >
-          Add a track and start enjoying...
-        </h3>
-      )}
-      <AddTrackButton />
-      {nowPlaying.length > 0 ? (
-        <AudioPlayer
-          src={URL.createObjectURL(new Blob(nowPlaying))}
-          autoPlay
-          volume="0.5"
-          onEnded={autoNextTrackSelector}
-        />
-      ) : null}
+      <TrackList />
+      <Controller />
     </div>
   );
 };
