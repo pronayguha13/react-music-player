68266b20702226bb045a76c8d31d2491374320f3
Pronay Guha <pronayguha13@gmail.com>
2021-08-12 22:44:57 +0530

    Header designed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 
+import { MusicContext } from "../../global/MusicContext";
 import styles from "./styles.module.css";
 
 const Header = () => {
-  return <div className={styles.Header}>ReactTrack-Play Your Jam</div>;
+  const { trackList } = useContext(MusicContext);
+  return (
+    <div className={styles.Header}>
+      <p className={styles.HeaderText}>
+        ReactTrack<span className={styles.subHeaderText}>-Play Your Jam</span>
+      </p>
+      {trackList && trackList.length ? (
+        <img src="/assets/addBtn.svg" alt="Add" className={styles.addBtn} />
+      ) : null}
+    </div>
+  );
 };
 
 export default Header;
